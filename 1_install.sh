be5713fe262c6708c5c9cdc9b303ef74686c767f
martin0641 <martin0641@gmail.com>
2020-11-03 17:00:35 -0500

    Update 1_install.sh

--- 1_install.sh
+++ 1_install.sh
@@ -257,7 +257,7 @@ Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 install-module -name VMware.PowerCLI -force
 'Set-PowerCLIConfiguration -Scope AllUsers -ParticipateInCeip \$false -Confirm:\$false -InvalidCertificateAction Ignore'
 install-module -name Posh-SSH
-Find-Module -Name vmware* | install-module
+#Find-Module -Name vmware* | install-module
 
 bash
 su anon
