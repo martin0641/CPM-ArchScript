d653440ba48a0d895709bbaf597956a0e95f4391
martin0641 <martin0641@gmail.com>
2020-11-10 20:10:53 -0500

    Update 1_install.sh

--- 1_install.sh
+++ 1_install.sh
@@ -219,23 +219,24 @@ sudo chown -R anon:anon /opt/S
 
 echo "Installing iperf3"
 cd /opt
-sudo git clone https://github.com/esnet/iperf.git
-sudo mv iperf iperf3
-sudo chown -R anon:anon ./iperf3
+sudo su
+git clone https://github.com/esnet/iperf.git
+mv iperf iperf3
+chown -R anon:anon ./iperf3
 cd /opt/iperf3
-sudo ./configure
-sudo make
-sudo make install
+./configure
+make
+make install
 
 echo "Installing iperf"
 cd /opt
-sudo git clone https://github.com/esnet/iperf.git
-sudo mv iperf/ iperf2-code
-sudo chown -R anon:anon ./iperf2-code
+git clone https://github.com/esnet/iperf.git
+mv iperf/ iperf2-code
+chown -R anon:anon ./iperf2-code
 cd /opt/iperf2-code
-sudo ./configure
-sudo make
-sudo make install
+./configure
+make
+make install
 
 su anon
 
