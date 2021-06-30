447b83aedcd649e1356404b3dbe14e118d5633a6
martin0641 <martin0641@gmail.com>
2021-06-30 17:33:36 -0400

    #tags
    #tags

--- 1_install.sh
+++ 1_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-encryption_passphrase="123!@#qweQWE"
+encryption_passphrase="bananas"
 root_password="123!@#qweQWE"
 user_password="123!@#qweQWE"
 hostname="archscripts"
@@ -100,7 +100,7 @@ yes | mkswap /dev/vg0/swap
 swapon /dev/vg0/swap
 
 echo "Installing Arch Linux"
-yes '' | pacstrap /mnt base base-devel linux linux-headers linux-lts linux-lts-headers linux-firmware lvm2 device-mapper e2fsprogs $cpu_microcode cryptsetup networkmanager wget man-db man-pages nano diffutils flatpak lm_sensors neofetch nmon lshw dhclient f2fs-tools git grub fish # freetype2 libglvnd man-db nano openssh screen vim which bonnie++ python atop sysstat nfs-utils open-iscsi multipath-tools open-vm-tools iperf time hdparm fio bc pv gnuplot msmtp mailx gptfdisk aurpublish lynx libzip oniguruma php tcl openmp
+yes '' | pacstrap /mnt base base-devel linux linux-headers linux-lts linux-lts-headers linux-firmware lvm2 device-mapper e2fsprogs $cpu_microcode cryptsetup networkmanager wget man-db man-pages nano diffutils flatpak lm_sensors neofetch nmon lshw dhclient f2fs-tools git grub fish freetype2 libglvnd man-db nano openssh screen vim which bonnie++ python atop sysstat nfs-utils open-iscsi multipath-tools open-vm-tools iperf time hdparm fio bc pv gnuplot msmtp mailx gptfdisk aurpublish lynx libzip oniguruma php tcl openmp
 
 echo "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -141,7 +141,7 @@ usermod -a -G video $user_name
 echo -en "$user_password\n$user_password" | passwd $user_name
 
 echo "Generating initramfs"
-sed -i 's/^HOOKS.*/HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block filesystems sd-encrypt sd-lvm2 filesystems f2fs fsck)/' /etc/mkinitcpio.conf
+sed -i 's/^HOOKS.*/HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block filesystems sd-encrypt lvm2 filesystems f2fs fsck)/' /etc/mkinitcpio.conf
 sed -i 's/^MODULES.*/MODULES=(ext4 $initramfs_modules)/' /etc/mkinitcpio.conf
 sed -i 's/#COMPRESSION="lz4"/COMPRESSION="lz4"/g' /etc/mkinitcpio.conf
 mkinitcpio -p linux
@@ -165,7 +165,9 @@ title Arch Linux
 linux /vmlinuz-linux
 initrd /$cpu_microcode.img
 initrd /initramfs-linux.img
-options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p2)=cryptlvm root=/dev/vg0/root resume=/dev/vg0/swap rd.luks.options=discard$kernel_options nmi_watchdog=0 quiet rw
+#options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p2)=cryptlvm root=/dev/vg0/root resume=/dev/vg0/swap rd.luks.options=discard$kernel_options nmi_watchdog=0 quiet rw
+options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p2)=cryptlvm root=/dev/vg0/root resume=/dev/vg0/swap rd.luks.options=nmi_watchdog=0 quiet rw
+
 END
 
 touch /boot/loader/entries/arch-lts.conf
@@ -174,7 +176,9 @@ title Arch Linux LTS
 linux /vmlinuz-linux-lts
 initrd /$cpu_microcode.img
 initrd /initramfs-linux-lts.img
-options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p2)=cryptlvm root=/dev/vg0/root resume=/dev/vg0/swap rd.luks.options=discard$kernel_options nmi_watchdog=0 quiet rw
+#options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p2)=cryptlvm root=/dev/vg0/root resume=/dev/vg0/swap rd.luks.options=discard$kernel_options nmi_watchdog=0 quiet rw
+options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p2)=cryptlvm root=/dev/vg0/root resume=/dev/vg0/swap rd.luks.options=nmi_watchdog=0 quiet rw
+
 END
 
 echo "Updating systemd-boot"
